13e9ef7093d90c8e6216b275afb6401d7293ebee
Charles Loges IV <c4webdev@gmail.com>
2021-05-10 20:33:09 -0400

    * style fixed spacing in script.js

--- script.js
+++ script.js
@@ -27,8 +27,7 @@ function tellMe(joke) {
 // Get jokes from joke api
 async function getJokes() {
   let joke = '';
-  const apiUrl =
-    'https://sv443.net/jokeapi/v2/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist';
+  const apiUrl = 'https://sv443.net/jokeapi/v2/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist';
   try {
     const response = await fetch(apiUrl);
     const data = await response.json();
